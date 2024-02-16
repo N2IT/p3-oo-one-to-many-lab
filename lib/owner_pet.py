471dce6b52dac4a2ed50356016480e634c7a4ed7
Tony Eder <tonyeder11@gmail.com>
2024-02-16 15:30:18 -0800

    validated that pet_type is one of the allowed PET_TYPES

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,26 @@
 class Pet:
-    pass
+    
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type=pet_type
+        else:
+            raise Exception
+        self.owner = owner
 
+    # @property
+    # def pet_type(self):
+    #     return self._pet_type
+
+    # @pet_type.setter
+    # def pet_type(self, pet_type):
+    #     if pet_type in Pet.PET_TYPES:
+    #         self._pet_type=pet_type
+    #     else:
+    #         raise Exception
+       
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
